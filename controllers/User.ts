b7356997df3d09be270edd1aa566aa050fe35d23
Tristan Derez <Tristan.derez@gmail.com>
2023-09-08 17:55:35 +0200

    use bcryptjs instead of bcrypt

--- controllers/User.ts
+++ controllers/User.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import userSuccess from "../controllers/UserSuccess";
 
