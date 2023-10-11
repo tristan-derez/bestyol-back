011ceb3b84116406991b67b9bb7859b33ef5daad
Tristan Derez <Tristan.derez@gmail.com>
2023-10-11 12:07:52 +0200

    add allowed origins as env

--- app.ts
+++ app.ts
@@ -13,9 +13,11 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const allowedOrigin: string = process.env.CORS_ALLOWED_ORIGIN as string;
+
 // cors middleware
 app.use((_req: Request, res: Response, next: NextFunction) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
