7008a5b4e306144494ce7dd0ca7e4939ddd4d874
Tristan Derez <Tristan.derez@gmail.com>
2023-10-11 21:53:53 +0200

    add cron job

--- server.ts
+++ server.ts
@@ -1,11 +1,19 @@
 import dotenv from "dotenv";
 import * as http from "http";
 import app from "./app";
+import cron from "node-cron";
+import { newActiveDaily } from "./utils/switchActiveStatus";
+
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+// call newActiveDaily everyday at 12:05am
+cron.schedule("5 0 * * *", () => {
+    newActiveDaily(6);
+});
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
