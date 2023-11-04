334ee39bd3c4c4f4d09311894e503836a664adff
Tristan Derez <Tristan.derez@gmail.com>
2023-11-04 20:28:45 +0100

    rename schemas and add params parsing + better handling of body parse

--- schemas/User.ts
+++ schemas/User.ts
@@ -26,21 +26,104 @@ export const LoginSchema = z.object({
     }),
 });
 
+export const GetUserSchema = z.object({
+    params: z.object({
+        userId: z.string().refine(
+            (value) => {
+                const parsedValue = parseInt(value, 10);
+                return !isNaN(parsedValue) && isFinite(parsedValue);
+            },
+            {
+                message: "Le paramètre userId doit être un nombre valide",
+                path: ["userId"],
+            }
+        ),
+    }),
+});
+
 export const EditUsernameEmailSchema = z.object({
+    params: z.object({
+        userId: z.string().refine(
+            (value) => {
+                const parsedValue = parseInt(value, 10);
+                return !isNaN(parsedValue) && isFinite(parsedValue);
+            },
+            {
+                message: "Le paramètre userId doit être un nombre valide",
+                path: ["userId"],
+            }
+        ),
+    }),
     body: z.object({
         username: z.string().max(usernameMaxChar, { message: usernameLengthError }).optional(),
         email: z.string().email({ message: emailError }).optional(),
     }),
 });
 
-export const EditUserPassword = z.object({
+export const EditUserPasswordSchema = z.object({
+    params: z.object({
+        userId: z.string().refine(
+            (value) => {
+                const parsedValue = parseInt(value, 10);
+                return !isNaN(parsedValue) && isFinite(parsedValue);
+            },
+            {
+                message: "Le paramètre userId doit être un nombre valide",
+                path: ["userId"],
+            }
+        ),
+    }),
+    body: z
+        .object({
+            formerPassword: z.string(),
+            newPassword: z.string().min(12, "Le mot de passe doit contenir au moins 12 caractères"),
+        })
+        .refine(
+            ({ formerPassword, newPassword }) => {
+                return formerPassword !== newPassword;
+            },
+            {
+                message: "Les mots de passe doivent être différents",
+                path: ["newPassword"], // You can specify the field you want to associate the error with
+            }
+        ),
+});
+
+export const EditUserPictureSchema = z.object({
+    params: z.object({
+        userId: z.string().refine(
+            (value) => {
+                const parsedValue = parseInt(value, 10);
+                return !isNaN(parsedValue) && isFinite(parsedValue);
+            },
+            {
+                message: "Le paramètre userId doit être un nombre valide",
+                path: ["userId"],
+            }
+        ),
+    }),
     body: z.object({
-        newPassword: z.string().min(12, "Le mot de passe doit contenir au moins 12 caractères"),
+        pictureNumber: z.number().refine((value) => value >= 1 && value <= 48, {
+            message: "Doit être un nombre entre 1 et 48",
+            path: ["pictureNumber"],
+        }),
     }),
 });
 
-export const EditUserPicture = z.object({
+export const DeleteUserSchema = z.object({
+    params: z.object({
+        userId: z.string().refine(
+            (value) => {
+                const parsedValue = parseInt(value, 10);
+                return !isNaN(parsedValue) && isFinite(parsedValue);
+            },
+            {
+                message: "Le paramètre userId doit être un nombre valide",
+                path: ["userId"],
+            }
+        ),
+    }),
     body: z.object({
-        pictureNumber: z.number(),
+        password: z.string({ invalid_type_error: "Password doit être une chaine de caractères", required_error: "Le mot de passe est requis" }),
     }),
 });
