ed052cd041c7512a7651a225cd2f0ed7e6608937
Tristan Derez <Tristan.derez@gmail.com>
2023-11-04 23:42:05 +0100

    create schema for usertasks

--- routes/UserTasks.ts
+++ routes/UserTasks.ts
@@ -3,22 +3,32 @@ import { verifyAuthToken } from "../middlewares/verifyAuthToken";
 
 const router: Router = express.Router();
 
-import userTasksController from "../controllers/UserTasks";
+import userTasksController, { deleteCustomTask } from "../controllers/UserTasks";
 import idValidation from "../middlewares/idValidation";
+import validateSchema from "../middlewares/validateSchema";
+import {
+    CreateUserCustomTaskSchema,
+    CreateUserDailyTasksSchema,
+    DeleteCustomTaskSchema,
+    GetUserTasksSchema,
+    UpdateCustomTaskSchema,
+    ValidateCustomTaskSchema,
+    ValidateDailyTaskSchema,
+} from "../schemas/UserTask";
 
 //* POST
-router.post("/:userId", [verifyAuthToken, idValidation], userTasksController.createUserCustomTask);
-router.post("/daily/:userId", [verifyAuthToken, idValidation], userTasksController.createUserDailyTasks);
+router.post("/:userId", [validateSchema(CreateUserCustomTaskSchema), verifyAuthToken, idValidation], userTasksController.createUserCustomTask);
+router.post("/daily/:userId", [validateSchema(CreateUserDailyTasksSchema), verifyAuthToken, idValidation], userTasksController.createUserDailyTasks);
 
 //* GET
-router.get("/:userId", [verifyAuthToken, idValidation], userTasksController.getUserTasks);
+router.get("/:userId", [validateSchema(GetUserTasksSchema), verifyAuthToken, idValidation], userTasksController.getUserTasks);
 
 //* PATCH
-router.patch("/:taskId", verifyAuthToken, userTasksController.changeTitleCustomTask);
-router.patch("/daily/:userTaskId", verifyAuthToken, userTasksController.validateDailyTask);
-router.patch("/custom/:userTaskId", verifyAuthToken, userTasksController.validateCustomTask);
+router.patch("/:taskId", [validateSchema(UpdateCustomTaskSchema), verifyAuthToken], userTasksController.changeTitleCustomTask);
+router.patch("/daily/:userTaskId", [validateSchema(ValidateDailyTaskSchema), verifyAuthToken], userTasksController.validateDailyTask);
+router.patch("/custom/:userTaskId", [validateSchema(ValidateCustomTaskSchema), verifyAuthToken], userTasksController.validateCustomTask);
 
 //* DELETE
-router.delete("/:taskId", verifyAuthToken, userTasksController.deleteCustomTask);
+router.delete("/:taskId", [validateSchema(DeleteCustomTaskSchema), verifyAuthToken], userTasksController.deleteCustomTask);
 
 export default router;
