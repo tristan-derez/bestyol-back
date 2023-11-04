68d948e0ad1345b6f28dab398d388a51d9ff7a01
Tristan Derez <Tristan.derez@gmail.com>
2023-11-04 21:24:28 +0100

    add validation of schema on every routes

--- schemas/Yol.ts
+++ schemas/Yol.ts
@@ -1,12 +1,63 @@
 import { z } from "zod";
 
-const yolMinChar: number = 2;
-const yolMaxChar: number = 20;
-const yolMinError: string = `Le nom du Yol doit contenir au moins ${yolMinChar} caractères`;
-const yolMaxError: string = `Le nom du Yol ne doit pas dépasser ${yolMaxChar} caractères`;
+const yolMinChar: number = 1;
+const yolMaxChar: number = 50;
+const yolLengthError: string = `Le nom du Yol doit contenir entre ${yolMinChar} et ${yolMaxChar} caractères`;
+const validSpeciesIds = [1, 5, 9];
 
-export const nameYolSchema = z.object({
+export const CreateYolSchema = z.object({
     body: z.object({
-        name: z.string().min(yolMinChar, { message: yolMinError }).max(yolMaxChar, { message: yolMaxError }),
+        name: z.string().min(yolMinChar, { message: yolLengthError }).max(yolMaxChar, { message: yolLengthError }),
+        userId: z.number({ invalid_type_error: "userId doit être une chaine de caractère", required_error: "Ce champ est requis" }),
+        speciesId: z
+            .number({ invalid_type_error: "speciesId doit être un nombre", required_error: "Ce champ est requis" })
+            .refine((value) => validSpeciesIds.includes(value), {
+                message: "speciesId doit être l'une des valeurs suivantes: 1, 5, 9",
+            }),
+    }),
+});
+
+export const GetOneYolSchema = z.object({
+    params: z.object({
+        yolId: z.string().refine(
+            (value) => {
+                const parsedValue = parseInt(value, 10);
+                return !isNaN(parsedValue) && isFinite(parsedValue);
+            },
+            {
+                message: "Ce paramètre doit être un nombre valide",
+                path: ["yolId"],
+            }
+        ),
+    }),
+});
+
+export const GetOneYolByUserIdSchema = z.object({
+    params: z.object({
+        userId: z.string().refine(
+            (value) => {
+                const parsedValue = parseInt(value, 10);
+                return !isNaN(parsedValue) && isFinite(parsedValue);
+            },
+            {
+                message: "Ce paramètre doit être un nombre valide",
+                path: ["userId"],
+            }
+        ),
+    }),
+});
+
+export const EvolveYolSchema = z.object({
+    params: z.object({
+        yolId: z.string().refine(
+            (value) => {
+                const parsedValue = parseInt(value, 10);
+                return !isNaN(parsedValue) && isFinite(parsedValue);
+            },
+            {
+                message: "Ce paramètre doit être un nombre valide",
+                path: ["yolId"],
+            }
+        ),
     }),
 });
